4d2ef8685f9815e0725da844306acb0d73caa074
rofixkron <rofo.kron@gmail.com>
2021-11-10 21:41:46 +0700

    Improvement Error Handling

--- src/News/News.jsx
+++ src/News/News.jsx
@@ -14,7 +14,7 @@ export default class NewsComponent extends React.Component{
 
       getArticles = (value) => {
         if(!value){
-          axios.get("https://newsapi.org/v2/top-headlines?country=id&apiKey=c313cc9772c14da19f0a8a18c1a32063")
+          axios.get("https://newsapi.org/v2/top-headliness?country=id&apiKey=c313cc9772c14da19f0a8a18c1a32063")
           .then(response => response.data.articles.map(article => ({
               image:`${article.urlToImage}`,
               title: `${article.title}`,
@@ -30,7 +30,19 @@ export default class NewsComponent extends React.Component{
               isLoading: false
             });
           })
-          .catch(error => this.setState({ errors:<h2>Mohon Maaf Sedang Dalam Perbaikan</h2>, isLoading: false }));
+          .catch(error => {
+            if(error.response){
+              console.log("Yang Error Data", error.response.data)
+              this.setState({ errors:<h2>Mohon Maaf Sedang Dalam Perbaikan</h2>, isLoading: false })
+            }else if(error.request){
+              console.log("Yang Error Request",error.request)
+            }else{
+              console.log('Errornya ', error.message)
+            }
+
+          console.log("Yang Error Config",error.config)
+        
+        });
 
         }else{                
           axios.get(`https://newsapi.org/v2/top-headlines?q=${value}&country=id&apiKey=c313cc9772c14da19f0a8a18c1a32063`)
